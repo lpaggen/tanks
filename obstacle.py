3dc4aed70a49feb10c702f0181c569c49a3e94a5
Futalose Leo <lpaggen@gmail.com>
2024-02-04 17:51:26 +0100

    working on animations

--- obstacle.py
+++ obstacle.py
@@ -13,6 +13,22 @@ class Obstacle(pygame.sprite.Sprite):
         self.id = identifier
         self.type = obstacletype
         self.pos = pygame.math.Vector2(x, y)
+        self.image_list = [["graphics/background/dirt_1.bmp", "graphics/background/dirt_2.bmp",
+                            "graphics/background/dirt_3.bmp", "graphics/background/dirt_4.bmp",
+                            "graphics/background/dirt_5.bmp", "graphics/background/dirt_6.bmp",
+                            "graphics/background/dirt_7.bmp", "graphics/background/dirt_8.bmp",
+                            "graphics/background/dirt_9.bmp", "graphics/background/dirt_10.bmp",
+                            "graphics/background/dirt_11.bmp", "graphics/background/dirt_12.bmp",
+                            "graphics/background/dirt_13.bmp", "graphics/background/dirt_14.bmp",
+                            "graphics/background/dirt_15.bmp", "graphics/background/dirt_16.bmp"],
+                            ["graphics/background/grass_1.bmp", "graphics/background/grass_2.bmp",
+                             "graphics/background/grass_3.bmp", "graphics/background/grass_4.bmp",
+                             "graphics/background/grass_5.bmp", "graphics/background/grass_6.bmp"],
+                             ["graphics/background/water_1_1.bmp", "graphics/background/water_1_2.bmp",
+                              "graphics/background/water_1_3.bmp", "graphics/background/water_1_4.bmp",
+                              "graphics/background/water_1_5.bmp", "graphics/background/water_1_6.bmp",
+                              "graphics/background/water_1_7.bmp", "graphics/background/water_1_8.bmp",
+                              "graphics/background/water_1_9.bmp", "graphics/background/water_1_10.bmp"]]
         self.image = pygame.transform.rotozoom(pygame.image.load("graphics/obstacles/obstacle.bmp").convert_alpha(), 0, 1.875)
         self.hitbox_rect = self.image.get_rect(center = self.pos)
         self.rect = self.hitbox_rect.copy()
@@ -28,6 +44,15 @@ class Obstacle(pygame.sprite.Sprite):
         self.v_right = (self.tright, self.bright)
         self.v_bottom = (self.bleft, self.bright)
         self.v_left = (self.bleft, self.tleft)
+
+        if self.type == 0: # wall obstacle
+            self.image = 
+        elif self.type == 1: # grass obstacle
+            pass
+        elif self.type == 2: # dirt obstacle
+            pass
+        elif self.type == 3: # water obstacle
+            pass
 
         Obstacle.obstaclesInstances.append(self)
         Obstacle.obstacle_group.add(self)
